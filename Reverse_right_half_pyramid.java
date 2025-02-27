388076321f1f7a7056a5d4f6f3ddc1c680c111a2
Nisha Gupta <ng8833909@gmail.com>
2025-02-27 18:38:58 +0530

    Add files via upload

---
+++ Reverse_right_half_pyramid.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Reverse_right_half_pyramid{
+    public static void main(String[] args){
+    
+        Scanner var1 = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = var1.nextInt();
+
+        for(int i = num; i > 0; i--){
+            for(int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
